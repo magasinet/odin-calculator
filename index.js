8601ad464c05211766faf501e4489feb27445061
Ali Sherifi <ali.sherifi@live.com>
2022-08-20 14:03:38 +0200

    Create basic math functions

---
+++ index.js
@@ -0,0 +1,19 @@
+// add
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+//subtract
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+//multiply
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+//divide
+function divide(num1, num2) {
+    return num1 / num2;
+}
