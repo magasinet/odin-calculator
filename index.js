08bd911a37889ec3d652446af5bca0aa59cf4e4f
Ali Sherifi <ali.sherifi@live.com>
2022-08-22 12:27:16 +0200

    Add values and display Add values and classes to calculator buttons Display clicked buttons

--- index.js
+++ index.js
@@ -22,3 +22,51 @@ const divide = function(num1, num2) {
 function operate(operator, num1, num2) {
     return operator(num1, num2);
 }
+
+
+let numbers = document.querySelectorAll('.number');
+
+/* numbers.forEach(numberBtn => {
+    numberBtn.addEventListener('click', () => {
+        console.log(numberBtn.value);
+    });
+}); */
+
+let operators = document.querySelectorAll('.operator');
+
+/* operators.forEach(operatorBtn => {
+    operatorBtn.addEventListener('click', () => {
+        console.log(operatorBtn.value);
+    });
+});
+ */
+
+let inputNumber = '';
+let clickedOperator = '';
+
+let display = document.querySelector('#display');
+
+let buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        display.textContent += button.textContent;
+        
+        //return button.value;
+        //displayValue += button.value;
+
+/*         if(button.classList.contains('number')) {
+            number += button.value;
+        } else if (button.classList.contains('operator')) {
+            operator = button.value;
+            number1 = number;
+            number = '';
+        } else if (button.classList.contains('equal')) {
+           
+            console.log(operate(add, parseFloat(number), parseFloat(number1)));
+        } */
+       
+
+    });
+});
+
+
